3a77cd7b582e79206b7549718d0dd76027ab8b77
Daniel-M-P <danieleduardomontejo@gmail.com>
2022-08-16 18:30:25 -0400

    Data persistence changes

--- Assets/Scripts/UI/UIMainScene.cs
+++ Assets/Scripts/UI/UIMainScene.cs
@@ -73,4 +73,9 @@ public class UIMainScene : MonoBehaviour
             InfoPopup.Name.text = content.GetName();
         }
     }
+
+    public void OpenMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
 }
